f368d9cd22ad28e3c0c34bf63d2fd0b24e445d38
priti620 <121668165+priti620@users.noreply.github.com>
2023-08-10 18:45:14 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,15 +2,15 @@
 
 output "bucket" {
   description = "The created storage bucket"
-  value       = google_storage_bucket.bucket
+  value       = google_storage_bucket.bucket1
 }
 
 output "name" {
   description = "Bucket name."
-  value       = google_storage_bucket.bucket.name
+  value       = google_storage_bucket.bucket1.name
 }
 
 output "url" {
   description = "Bucket URL."
-  value       = google_storage_bucket.bucket.url
+  value       = google_storage_bucket.bucket1.url
 }
